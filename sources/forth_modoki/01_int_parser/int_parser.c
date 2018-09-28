d89a7d1df6dd44666b086caed4b0052ab0cd848f
yohei-kikuta <yohei-kikuta@cookpad.com>
2018-09-28 14:46:52 +0900

    Tackle 01_int_parser
    Tackled 01_int_parser problem.

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -10,7 +10,45 @@ int main() {
     int answer3 = 0;
 
     // write something here.
+    int start = 0;
+    int end = 0;
+    int finish_flg = 0;
 
+    int answers[3];
+    int idx = 0;
+
+    while (finish_flg != 1) {
+        for (int i = start; input[i] != '\0'; i++) {
+            if (input[i] != ' ') {
+                start = i;
+                for (int j = start; input[j] != '\0'; j++) {
+                    if (input[j] == ' ') {
+                        end = j - 1; // one character before the space.
+                        break;
+                    }
+                    else {
+                        end = j;
+                        finish_flg = 1;
+                    }
+                }
+
+                // store splited strings into arrays.
+                int length = end - start;
+                char substr[length];
+                for (int k = 0; k <= length; k++){
+                    substr[k] = input[start+k];
+                }
+                answers[idx] = atoi(substr);
+                idx += 1;
+
+                // proceed with loop index by strings length
+                i += end - start;
+            }
+        }
+    }
+    answer1 = answers[0];
+    answer2 = answers[1];
+    answer3 = answers[2];
 
     // verity result.
     assert(answer1 == 123);
