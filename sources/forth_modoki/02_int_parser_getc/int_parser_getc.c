85c0bec69a0854c34cb842967db0d283d95fbee4
Yohei Kikuta <diracdiego@gmail.com>
2018-10-09 22:05:14 +0900

    Modify int_parser_getc.c following comments
    Change variable names.
    Make if conditions clearer.
    Remove redundunt expressions.

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -14,16 +14,18 @@ int main() {
     // Initialization.
     enum TokenType out_token_type;
     int out_token_value = 0;
-    int next_str_int = 0;
+    int head_char = '\0';
     int answers[2];
-
-    // Read the first string integer.
-    next_str_int = cl_getc();
 
     // Parse one by one.
     int idx = 0;
-    while (next_str_int != EOF) {
-        next_str_int = parse_one(next_str_int, &out_token_type, &out_token_value);
+    while (head_char != EOF) {
+        // Read the first character.
+        if (head_char == '\0') {
+            head_char = cl_getc();
+        }
+
+        head_char = parse_one(head_char, &out_token_type, &out_token_value);
         if (out_token_type == NUMBER) {
             answers[idx] = out_token_value;
             idx++;
@@ -43,41 +45,37 @@ int main() {
 }
 
 
-int parse_one(int input_int, enum TokenType *out_token_type, int *out_token_value) {
+int parse_one(int head_char, enum TokenType *out_token_type, int *out_token_value) {
     *out_token_type = 0;
     *out_token_value = 0;
-    enum TokenType val_type;
-    int str_int = 0;
+    int cur_char = 0;
 
-    // Set the token type of the input integer.
-    if (input_int == ' ') {
-        val_type = SPACE;
-    } else {
-        val_type = NUMBER;
+    // Parse one unit for each head_char type.
+    if (head_char == ' ') {
+        *out_token_value = ' ';
+        *out_token_type = SPACE;
+
+        // Move to the next digit chracter.
+        while ((cur_char = cl_getc()) != EOF) {
+            if (('0' <= cur_char) && (cur_char <= '9')) {
+                return cur_char;
+            }
+        }
+    } else if (('0' <= head_char) && (head_char <= '9')) {
+        // Set the first number come from the head character.
+        *out_token_value = (head_char - '0');
+        *out_token_type = NUMBER;
+
+        // Compute output integer until reaching to the next space string.
+        // Since while loop continues to the next space character,
+        // out_token_value has to be computed after if condition.
+        while ((cur_char = cl_getc()) != EOF) {
+            if (cur_char == ' ') {
+                return cur_char;
+            }
+            *out_token_value = (10 * *out_token_value) + (cur_char - '0');
+        }
     }
 
-    // Parse one unit.
-    switch (val_type) {
-        case SPACE:
-            *out_token_value = ' ' - 0;
-            *out_token_type = SPACE;
-            // Move to the next non-space string.
-            while ((str_int = cl_getc()) != EOF) {
-                if (str_int != ' ') {break;}
-            }
-            break;
-        case NUMBER:
-            // Set the first number come from the input integer.
-            *out_token_value = (input_int - '0');
-            *out_token_type = NUMBER;
-            // Compute output integer until reaching to the next space string.
-            while ((str_int = cl_getc()) != EOF) {
-                if (str_int == ' ') {break;}
-                *out_token_value = (10 * *out_token_value) + (str_int - '0');
-            }
-            break;
-    }
-
-    // Return the next string integer.
-    return str_int;
+    return cur_char;
 }
