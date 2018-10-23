ff64ec12d62e98105ebd83acfb824356497dd4a6
Yohei Kikuta <diracdiego@gmail.com>
2018-10-23 22:33:40 +0900

    Add assertion function for unittest

--- sources/forth_modoki/04_stack/stack.c
+++ sources/forth_modoki/04_stack/stack.c
@@ -35,17 +35,21 @@ static int streq(char *s1, char *s2) {
     return strcmp(s1, s2) == 0;
 }
 
-static int compare_two_data(struct Data d1, struct Data d2) {
+static int two_data_eq(struct Data *d1, struct Data *d2) {
     // Return 1 if two Data are identical.
-    if (d1.dtype == d2.dtype) {
-        if (d1.dtype == NUMBER) {
-            return d1.u.number == d2.u.number;
-        } else if (d1.dtype == LITERAL_NAME) {
-            return streq(d1.u.name, d2.u.name);
+    if (d1->dtype == d2->dtype) {
+        if (d1->dtype == NUMBER) {
+            return d1->u.number == d2->u.number;
+        } else if (d1->dtype == LITERAL_NAME) {
+            return streq(d1->u.name, d2->u.name);
         }
     } else {
         return 0;
     }
+}
+
+static void assert_two_data_eq(struct Data *d1, struct Data *d2) {
+    assert(two_data_eq(d1, d2));
 }
 
 static void test_pop_from_empty() {
@@ -66,7 +70,7 @@ static void test_one_push() {
     struct Data actual_data = stack[0];
 
     assert(expect == actual);
-    assert(compare_two_data(expect_data, actual_data));
+    assert_two_data_eq(&expect_data, &actual_data);
     reset_stuck_num();
 }
 
@@ -79,7 +83,7 @@ static void test_one_push_one_pop() {
     struct Data actual_data_pop;
     int actual = stack_pop(&actual_data_pop);
 
-    assert(compare_two_data(expect_data_pop, actual_data_pop));
+    assert_two_data_eq(&expect_data_pop, &actual_data_pop);
     reset_stuck_num();
 }
 
@@ -95,14 +99,14 @@ static void test_two_push_two_pop() {
     stack_push(&data_push_2);
     struct Data actual_data_1 = stack[0];
     struct Data actual_data_2 = stack[1];
-    assert(compare_two_data(expect_data_1, actual_data_1));
-    assert(compare_two_data(expect_data_2, actual_data_2));
+    assert_two_data_eq(&expect_data_1, &actual_data_1);
+    assert_two_data_eq(&expect_data_2, &actual_data_2);
 
     struct Data actual_data_pop_1, actual_data_pop_2;
     stack_pop(&actual_data_pop_2);
     stack_pop(&actual_data_pop_1);
-    assert(compare_two_data(expect_data_pop_1, actual_data_pop_1));
-    assert(compare_two_data(expect_data_pop_2, actual_data_pop_2));
+    assert_two_data_eq(&expect_data_pop_1, &actual_data_pop_1);
+    assert_two_data_eq(&expect_data_pop_2, &actual_data_pop_2);
 
     reset_stuck_num();
 }
@@ -115,6 +119,6 @@ int main() {
         {LITERAL_NAME, .u.name = "abc"}
     };
     struct Data test2 = {LITERAL_NAME, .u.name = "abc"};
-    assert(compare_two_data(test1[1], test2));
+    assert_two_data_eq(&test1[1], &test2);
     return 1;
 }
