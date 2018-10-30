78af2265a1a87fab6175996f1c95c18870ec02ae
Yohei Kikuta <diracdiego@gmail.com>
2018-10-30 21:49:24 +0900

    Add NUMBER handling in eval
    At this time eval can handle only NUMBER token.
    Also stack.h uses enum defined in parser.h,
    which maybe should be replaced with more suitable one.

--- sources/forth_modoki/05_eval_num/eval.c
+++ sources/forth_modoki/05_eval_num/eval.c
@@ -3,7 +3,28 @@
 #include "stack.h"
 #include <assert.h>
 
-void eval() {}
+void eval() {
+    int ch = EOF;
+    struct Token token = {
+        UNKNOWN,
+        {0}
+    };
+
+    do {
+        ch = parse_one(ch, &token);
+        if (token.ltype != UNKNOWN) {
+            switch (token.ltype) {
+                case NUMBER:
+                    stack_push(&token);
+                    break;
+
+                default:
+                    printf("Unknown type %d\n", token.ltype);
+                    break;
+            }
+        }
+    } while (ch != EOF);
+}
 
 static void test_eval_num_one() {
     char *input = "123";
@@ -15,6 +36,9 @@ static void test_eval_num_one() {
 
     /* TODO: write code to pop stack top element */
     int actual = 0;
+    struct Data dummy = {UNKNOWN, {0}};
+    stack_pop(&dummy);
+    actual = dummy.u.number;
 
     assert(expect == actual);
 
@@ -36,7 +60,6 @@ static void test_eval_num_two() {
     assert(expect1 == actual1);
     assert(expect2 == actual2);
 }
-
 
 static void test_eval_num_add() {
     char *input = "1 2 add";
