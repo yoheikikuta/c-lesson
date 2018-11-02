89f051d733913ce924a94e5589af03b265e2c764
Yohei Kikuta <diracdiego@gmail.com>
2018-11-03 00:31:22 +0900

    Move two_data_eq into cl_util

--- sources/forth_modoki/interpreter/cl_util.c
+++ sources/forth_modoki/interpreter/cl_util.c
@@ -1,6 +1,19 @@
 #include <string.h>
 #include "cl_util.h"
+#include "stack.h"
 
 int streq(char *s1, char *s2) {
     return strcmp(s1, s2) == 0;
+}
+
+int two_data_eq(struct Data *d1, struct Data *d2) {
+    if (d1->dtype == d2->dtype) {
+        if (d1->dtype == NUMBER) {
+            return d1->u.number == d2->u.number;
+        } else if (d1->dtype == LITERAL_NAME) {
+            return streq(d1->u.name, d2->u.name);
+        }
+    } else {
+        return 0;
+    }
 }
