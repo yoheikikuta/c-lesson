4ca61be97b22c41ab1bb05593e8845e49713691b
Yohei Kikuta <diracdiego@gmail.com>
2018-11-03 00:31:22 +0900

    Move two_data_eq into cl_util

--- sources/forth_modoki/interpreter/cl_util.h
+++ sources/forth_modoki/interpreter/cl_util.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <stdio.h>
+#include "stack.h"
 
 /*
 return 1 if two strings are identical.
+return 1 if two Datas are identical.
 */
 int streq(char *s1, char *s2);
+int two_data_eq(struct Data *d1, struct Data *d2);
