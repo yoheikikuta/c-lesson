dce760cf15f1061c7179f9b62be43c81928fa8ff
Yohei Kikuta <diracdiego@gmail.com>
2018-11-05 23:35:51 +0900

    [Refactoring] Refactor find_key_index in dict.c
    To clarify the positon of index clearly.

--- sources/forth_modoki/interpreter/dict.c
+++ sources/forth_modoki/interpreter/dict.c
@@ -15,8 +15,10 @@ static struct KeyValue dict_array[DICT_SIZE];
 static int find_key_index(char* key, int* out_index) {
     *out_index = 0;
     for (int i=0; i < dict_pos; i++) {
-        if (streq(dict_array[i].key, key)) {return 1;}
-        (*out_index)++;
+        if (streq(dict_array[i].key, key)) {
+            *out_index = i;
+            return 1;
+        }
     }
     return 0;
 }
