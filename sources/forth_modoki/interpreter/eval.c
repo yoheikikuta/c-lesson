30b39aaa199460f66f761ff48db0933cb76cef82
yoheikikuta <diracdiego@gmail.com>
2019-04-01 01:37:32 +0900

    [Refactoring] Extract primitives and create primitive.c to define them
    Since we plan to define many primitives,
    create primitive.c to define them.

--- sources/forth_modoki/interpreter/eval.c
+++ sources/forth_modoki/interpreter/eval.c
@@ -4,6 +4,7 @@
 #include "dict.h"
 #include "parser.h"
 #include "stack.h"
+#include "primitive.h"
 
 
 int stack_pop_int(){
@@ -22,68 +23,6 @@ char* stack_pop_str(){
     char* str = input.u.name;
 
     return str;
-}
-
-void def_op() {
-    // def operation: [{ELEMENT_LITERAL_NAME,"abc"}, {ELEMENT_NUMBER,123}] -> dict[{"abc", {ELEMENT_NUMBER,123}}]
-    struct Element elem = {NO_ELEM_TYPE,{0}};
-	struct Element elem_val = {NO_ELEM_TYPE,{0}};
-	char* literal_name;
-	stack_pop(&elem);
-	elem_val = elem;
-	stack_pop(&elem);
-	literal_name = elem.u.name;
-
-    dict_put(literal_name, &elem_val);
-}
-
-void add_op() {
-    // Add operation: [{ELEMENT_NUMBER,1}, {ELEMENT_NUMBER,2}] -> [{ELEMENT_NUMBER,3}]
-    int v1 = stack_pop_int();
-    int v2 = stack_pop_int();
-
-    struct Element result = {ELEMENT_NUMBER, {v1+v2}};
-    stack_push(&result);
-}
-
-void sub_op() {
-    // Sub operation: [{ELEMENT_NUMBER,5}, {ELEMENT_NUMBER,2}] -> [{ELEMENT_NUMBER,3}]
-    int v1 = stack_pop_int();
-    int v2 = stack_pop_int();
-
-    struct Element result = {ELEMENT_NUMBER, {v2-v1}};
-    stack_push(&result);
-}
-
-void mul_op() {
-    // Mul operation: [{ELEMENT_NUMBER,2}, {ELEMENT_NUMBER,4}] -> [{ELEMENT_NUMBER,8}]
-    int v1 = stack_pop_int();
-    int v2 = stack_pop_int();
-
-    struct Element result = {ELEMENT_NUMBER, {v1*v2}};
-    stack_push(&result);
-}
-
-void div_op() {
-    // Div operation: [{ELEMENT_NUMBER,5}, {ELEMENT_NUMBER,2}] -> [{ELEMENT_NUMBER,2}]
-    int v1 = stack_pop_int();
-    int v2 = stack_pop_int();
-
-    struct Element result = {ELEMENT_NUMBER, {v2/v1}};
-    stack_push(&result);
-}
-
-void register_one_primitive(char* op_name, void (*cfunc)(void)) {
-    struct Element opelem = {ELEMENT_C_FUNC, {.cfunc = cfunc}};
-    dict_put(op_name, &opelem);
-}
-
-void register_all_primitive() {
-    register_one_primitive("def", def_op);
-    register_one_primitive("add", add_op);
-    register_one_primitive("sub", sub_op);
-    register_one_primitive("mul", mul_op);
-    register_one_primitive("div", div_op);
 }
 
 void compile_exec_array(int ch, struct Token* token, struct Element* out_opelem) {
@@ -545,6 +484,10 @@ static void test_eval_executable_array_literal_name_bind_nest() {
     assert_two_exec_opelem_eq(&expect4, &actual4);
     assert_two_exec_opelem_eq(&expect5, &actual5);
     assert_two_exec_opelem_eq(&expect6, &actual6);
+}
+
+static void test_eval_eq() {
+    return;
 }
 
 static void unit_tests() {
