ad5a0576e666bff321c0c49deb70a8db94a87e26
yoheikikuta <diracdiego@gmail.com>
2019-04-21 16:52:04 +0900

    Implement print_hex.s
    This version uses unefficient copy and pastes.
    It should be modified later.
    Each byte is added by 0x37, which means only a to f of a hex number
    is correctly translated (0 to 9 is not correctly translated).

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -6,9 +6,46 @@
 */
 .globl _start
 _start:
+    ldr r0,=0x101f1000
     ldr r1,=0xdeadbeaf
+    mov r2,#28  @Initial value of bit rotation
     b print_hex
 print_hex:
-    // TODO: implement here
+    lsr r3,r1,r2
+    and r3,r3,#0x0F
+    add r3,r3,#0x37 @e.g., 13 + 0x37 -> 0x44 : D
+    str r3,[r0]
+    lsr r3,r1,#24
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    lsr r3,r1,#20
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    lsr r3,r1,#16
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    lsr r3,r1,#12
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    lsr r3,r1,#8
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    lsr r3,r1,#4
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    lsr r3,r1,#0
+    and r3,r3,#0x0F
+    add r3,r3,#0x37
+    str r3,[r0]
+    mov r3,#0x0D
+    str r3,[r0]
+    mov r3,#0x0A
+    str r3,[r0]
 end:
     b end
