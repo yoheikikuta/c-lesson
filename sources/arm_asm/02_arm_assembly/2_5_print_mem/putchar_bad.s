b050ebb3db507ac6a3db49fb6e26169f696511e4
yoheikikuta <diracdiego@gmail.com>
2019-04-22 22:36:23 +0900

    Implement putchar_bad.s
    Follow the instructions on the web.

--- sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_bad.s
+++ sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_bad.s
@@ -22,19 +22,19 @@ putchar:
 /*
   print:
     arg r0: Address of target string, must end by \0.
-    used internal register: r0, r1, r3.
+    used internal register: r0, r1, r2, r4.
 */
 print:
-  ldrb r3,[r0]
+  mov r2, r0
+  mov r4, r14
+  ldrb r0, [r0]
 _loop:
-  // TODO: use putchar here someway.
-  //
-
-  add r0, r0, #1
-  ldrb r3,[r0]
-  cmp r3,#0
+  bl putchar
+  add r2, r2, #1
+  ldrb r0, [r2]
+  cmp r0, #0
   bne _loop
-  mov r15, r14
+  mov r15, r4
 
 msg1: .asciz "First text.\n"
 msg2: .asciz "Second text!\n"
