cc493c380099dba8dd06de27a8eeff89aaaed1c8
yoheikikuta <diracdiego@gmail.com>
2019-04-22 09:32:38 +0900

    Implement print_hex_bl.s
    Implementation of print_hex follows the result of 2_3.

--- sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
+++ sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
@@ -1,9 +1,23 @@
 .globl _start
+
+/*
+r0  : Input of print_hex 
+r1  : UART address
+r2  : Bit rotation number used in lsr (0-32)
+r3  : Each 4 bits of r0 to be printed
+r14 : Link register used in bl
+r15 : Program counter
+*/
+
 _start:
 
+  ldr r1,=0x101f1000
+
+  mov r2, #32
   mov r0, r15
   bl print_hex
 
+  mov r2, #32
   mov r0, #0x68
   bl print_hex
 
@@ -12,4 +26,24 @@ end:
 
 
 print_hex:
-  // TODO: write here!
+  sub r2, r2, #4  @ Reset r2 to move to the next 4 bits
+  lsr r3, r0, r2
+  and r3, r3, #0x0F
+  cmp r3, #9
+  ble print_one_hex
+  add r3, r3, #0x27  @ For alphabet case
+
+print_one_hex:
+  add r3, r3, #0x30
+  str r3, [r1]
+  cmp r2, #0
+  bgt print_hex
+
+  @ Line breaks for the visibility
+  mov r3, #0x0D
+  str r3, [r1]
+  mov r3, #0x0A
+  str r3, [r1]
+
+  @ Go to the next address
+  mov r15, r14
