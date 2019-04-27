f84534d4bb176adff4ee21f0de4deaa46e8241ac
yoheikikuta <diracdiego@gmail.com>
2019-04-28 01:44:20 +0900

    Implement str case of any dest register case
    Also add an unittest of str with r2 destination register.

--- sources/arm_asm/04_disasm/cl_utils.c
+++ sources/arm_asm/04_disasm/cl_utils.c
@@ -80,8 +80,11 @@ int print_asm(int word) {
         offset_v = abs(offset_v << 2);  // ARM specifications
         cl_printf("b [r15, %s%x]\n", offset_s, offset_v);
         return 1;
-    } else if(word == 0xE5801000) {
-        cl_printf("str r1, [r0]\n");
+    } else if(0xE5800000 == (word & 0xE5800000)) {
+        // STR: str rX, [r0]
+        // Breakdown of 32 bits: [4 bits] 01 [10 bits] [dest register 4 bit] [12 bits]
+        int register_v = (word & 0x0000F000) >> 4*3;
+        cl_printf("str r%i, [r0]\n", register_v);
         return 1;
     }
     
@@ -164,6 +167,18 @@ static void test_print_asm_str() {
     cl_clear_output();
 }
 
+static void test_print_asm_str_dest_r2() {
+    int input = 0xE5802000;
+    char* expect = "str r2, [r0]\n";
+
+    char* actual;
+    print_asm(input);
+    actual = cl_get_result(0);
+
+    assert_two_str_eq(expect, actual);
+    cl_clear_output();
+}
+
 static void test_print_asm_not_instruction() {
     int input = 0x64646464;
     int expect = 0;
@@ -183,6 +198,7 @@ static void unit_tests() {
     test_print_asm_b_negative();
     test_print_asm_b_positive();
     test_print_asm_str();
+    test_print_asm_str_dest_r2();
     test_print_asm_not_instruction();
     printf("All unittests successfully passed.\n");
 
