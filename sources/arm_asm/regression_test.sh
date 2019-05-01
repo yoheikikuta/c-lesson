b75159cf3419af78dc625e4201fcf2853ef2947c
yoheikikuta <diracdiego@gmail.com>
2019-05-01 13:46:20 +0900

    Modify regression test to include print_loop

--- sources/arm_asm/regression_test.sh
+++ sources/arm_asm/regression_test.sh
@@ -1,13 +1,14 @@
 gcc -w ./04_disasm/cl_utils.c ./04_disasm/disassemble.c -o ./04_disasm/disassemble.out
 
-EXPECT_FILE="./04_disasm/test/test_expect/hello_arm.txt"
-ACTUAL_FILE="./04_disasm/test/test_input/hello_arm.bin"
+TARGETS=("hello_arm" "print_loop")
 
-DIFF=`diff <(cat $EXPECT_FILE) <(./04_disasm/disassemble.out $ACTUAL_FILE)`
+for TARGET in ${TARGETS[@]}; do
+	DIFF=`diff <(cat "./04_disasm/test/test_expect/$TARGET.txt") <(./04_disasm/disassemble.out "./04_disasm/test/test_input/$TARGET.bin")`
 
-if [ "$DIFF" = "" ]; then
-    echo "Regression test SUCESS: $ACTUAL_FILE"
-else
-    echo "Regression test FAIL: $ACTUAL_FILE"
-    echo $DIFF
-fi
+	if [ "$DIFF" = "" ]; then
+    	echo "Regression test SUCESS: $TARGET"
+	else
+    	echo "Regression test FAIL: $TARGET"
+    	echo $DIFF
+	fi
+done
