2a7c921317ae442fd5a6c5c9aaa90ff919d73caa
yoheikikuta <diracdiego@gmail.com>
2019-05-03 00:00:23 +0900

    Reimplement stmdb to be able to handle stmfd r13!, {r1, r12}
    Define a utility function to get registers list from
    the lower 16 bits.

--- sources/arm_asm/04_disasm/cl_utils.h
+++ sources/arm_asm/04_disasm/cl_utils.h
@@ -8,6 +8,7 @@ return the char pointer of a string stored in the buffer
 Enable the buffer mode
 Disable the buffer mode
 Store a string in the buffer if in the buffer mode, stdout else
+return pointer of str of registers, such as "r2, r8, r12"
 */
 int streq(char* s1, char* s2);
 void assert_two_str_eq(char* s1, char* s2);
@@ -16,3 +17,4 @@ char *cl_get_result(int num);
 void cl_enable_buffer_mode();
 void cl_disable_buffer_mode();
 void cl_printf(char *fmt, ...);
+char *get_registers_str_from_lower_16bits(int word);
