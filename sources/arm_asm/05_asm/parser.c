12de466ccc11e96de065b4b182eb0ba477ee5e15
yoheikikuta <diracdiego@gmail.com>
2019-05-11 12:04:50 +0900

    [Fix] Fix smart assert functions
    Forget to add assert in the previous version.
    Also modify the name of the unittest.

--- sources/arm_asm/05_asm/parser.c
+++ sources/arm_asm/05_asm/parser.c
@@ -16,13 +16,14 @@ int parse_register(char *str, int out_register);
 int skip_comma(char *str);
 
 void assert_two_num_eq(int num1, int num2) {
-    return num1 == num2;
+    return assert(num1 == num2);
 }
 
 void assert_str_substr_eq(char* str, struct Substring* substr) {
     char extracted_substr[sizeof(char) * substr->len];
     strncpy(&extracted_substr, substr->str, substr->len);
-    return strcmp(str, extracted_substr) == 0;
+
+    return assert(strcmp(str, extracted_substr) == 0);
 }
 
 // Return the length of str up to where the parse read.
@@ -38,7 +39,7 @@ int parse_one(char *str, struct Substring* out_subs) {
 // TEST
 // 
 
-static void test_parse_one_movr1r2() {
+static void test_parse_one_movr1r2_mov() {
 	char *input = "mov r1, r2";
 	char *expect = "mov";
 	int expect_len_read = 3;
@@ -52,6 +53,6 @@ static void test_parse_one_movr1r2() {
 }
 
 int main(int argc, char* argv[]) {
-    test_parse_one_movr1r2();
+    test_parse_one_movr1r2_mov();
     return 0;
 }
