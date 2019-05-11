c5abb942891933452c621dfdab7609b67757de2e
yoheikikuta <diracdiego@gmail.com>
2019-05-12 01:05:55 +0900

    Arrange files
    Add .c file for utility functions.
    Add some header files.
    Format some codes.

--- sources/arm_asm/05_asm/cl_getline.h
+++ sources/arm_asm/05_asm/cl_getline.h
@@ -3,9 +3,12 @@
 
 #define BUF_SIZE 256
 
+static char buf[BUF_SIZE] = {'\0'};
+static FILE* fp = NULL;
+
 /*
 return the length of the line and pack strings.
 set input file pointer.
 */
-int cl_getline(char **out_buf);
+int cl_getline(char** out_buf);
 void cl_getline_set_file(FILE* input_fp);
---
+++ sources/arm_asm/05_asm/cl_util.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "parser.h"
+
+void assert_two_num_eq(int num1, int num2);
+void assert_str_substr_eq(char* str, struct Substring* substr);
