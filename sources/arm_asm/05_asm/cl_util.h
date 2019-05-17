491d13051f1ba7fe8bc585256ccfd22ac5c42098
yoheikikuta <diracdiego@gmail.com>
2019-05-17 21:18:35 +0900

    Implement parse_str to parse string
    The function parse and get the string " "something" " part in a given string.
    Also add assert_two_str_eq utility function to compare two strings.

--- sources/arm_asm/05_asm/cl_util.h
+++ sources/arm_asm/05_asm/cl_util.h
@@ -4,9 +4,11 @@
 
 /*
 Assert if num1 != num2.
+Assert if str1 != str2.
 Assert if str != substr.
 Return the file pointer of a given relative file path.
 */
 void assert_two_num_eq(int num1, int num2);
+void assert_two_str_eq(char* str1, char* str2);
 void assert_str_substr_eq(char* str, struct Substring* substr);
 FILE* get_fp(char* rel_path);
