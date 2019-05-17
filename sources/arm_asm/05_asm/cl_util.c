c4fb665427b412e2e069a1e63b5ff8078f368e3e
yoheikikuta <diracdiego@gmail.com>
2019-05-17 21:18:35 +0900

    Implement parse_str to parse string
    The function parse and get the string " "something" " part in a given string.
    Also add assert_two_str_eq utility function to compare two strings.

--- sources/arm_asm/05_asm/cl_util.c
+++ sources/arm_asm/05_asm/cl_util.c
@@ -7,6 +7,10 @@ void assert_two_num_eq(int num1, int num2) {
     return assert(num1 == num2);
 }
 
+void assert_two_str_eq(char* str1, char* str2) {
+    return assert(strcmp(str1, str2) == 0);
+}
+
 void assert_str_substr_eq(char* str, struct Substring* substr) {
     char extracted_substr[BUF_SIZE] = {'\0'};
     strncpy(&extracted_substr, substr->str, substr->len);
