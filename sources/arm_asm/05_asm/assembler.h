1382d9978b98ac5a1a05e748a362527ec8b17a4a
yoheikikuta <diracdiego@gmail.com>
2019-05-17 14:16:30 +0900

    [Refactoring] Create Element struct to store words
    Also add char* str to the union of Element  to store strings,
    which is later used.

--- sources/arm_asm/05_asm/assembler.h
+++ sources/arm_asm/05_asm/assembler.h
@@ -3,7 +3,21 @@
 #define ASM_FAILURE -1
 #define WORD_BUF_SIZE 256 * 100
 
+enum ElementType {
+    NO_ELEM_TYPE,
+    ELEMENT_NUMBER,
+    ELEMENT_STRING
+};
+
+struct Element {
+    enum ElementType etype;
+    union {
+        int number;
+        char* str;
+    } u;
+};
+
 struct Emitter {
-    int word_buf[WORD_BUF_SIZE];
+    struct Element* elems;
     int pos;
 };
