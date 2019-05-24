0c60a481b4d221470ab7d68ac00e70b86963d0f4
yoheikikuta <diracdiego@gmail.com>
2019-05-24 17:07:55 +0900

    [Fix] Fix add node and add tests for to_mnemonic_symbol
    In the previous version, we used char array and
    its size showed unexpectedly behavior due to using function/variable.
    Fix to use char pointer and malloc to avoid the problem.
    Add some tests of to_mnemonic_symbol.

--- sources/arm_asm/05_asm/binary_tree.c
+++ sources/arm_asm/05_asm/binary_tree.c
@@ -19,6 +19,16 @@ int mnemonic_id = MNEMONIC_ID_INIT;
 int label_id = LABEL_ID_INIT;
 
 
+void reset_trees() {
+    mnemonic_root.right = NULL;
+    mnemonic_root.left = NULL;
+    mnemonic_id = MNEMONIC_ID_INIT;
+
+    label_root.right = NULL;
+    label_root.left = NULL;
+    label_id = LABEL_ID_INIT;
+}
+
 int find_node(char* str, struct Node* node) {
 	int str_compared = strncmp(str, node->name, strlen(node->name));
 
@@ -48,11 +58,13 @@ int add_node(char* str, int value, struct Node* node) {
 		if(node->right != NULL) {
 			return add_node(str, node->right, value);
 		} else {
-			char str_copied[strlen(str) + 1];
-            strcpy(&str_copied, str);
+            char* str_copied;
+            int len_str = strlen(str) + 1;
+            str_copied = malloc(len_str);
+            strcpy(str_copied, str);
 			
 			struct Node* node_right = malloc(sizeof(struct Node));
-			node_right->name = &str_copied;
+			node_right->name = str_copied;
 			node_right->value = value;
 			node_right->left = NULL;
 			node_right->right = NULL;
@@ -64,11 +76,13 @@ int add_node(char* str, int value, struct Node* node) {
 		if(node->left != NULL) {
 			return add_node(str, node->left, value);
 		} else {
-			char str_copied[strlen(str) + 1];
-            strcpy(&str_copied, str);
+            char* str_copied;
+            int len_str = strlen(str) + 1;
+            str_copied = malloc(len_str);
+            strcpy(str_copied, str);
 
 			struct Node *node_left = malloc(sizeof(struct Node));
-			node_left->name = &str_copied;
+			node_left->name = str_copied;
 			node_left->value = value;
 			node_left->left = NULL;
 			node_left->right = NULL;
@@ -85,7 +99,7 @@ int to_mnemonic_symbol(char* str) {
 	value = find_node(str, &mnemonic_root);
 	if(value == NO_EXISTING_NODE) {
 		mnemonic_id++;
-		value = add_node(str, &mnemonic_root, mnemonic_id);
+		value = add_node(str, mnemonic_id, &mnemonic_root);
 		return value;
 	} else {
 		return value;
@@ -149,6 +163,64 @@ static void test_add_node_new_node() {
 	assert_two_num_eq(expect, actual);
 }
 
+static void test_add_node_two_nodes() {
+	char* input_str_1 = "str";
+    int input_value_1 = 2;
+	char* input_str_2 = "ldr";
+    int input_value_2 = 3;
+    struct Node node = mnemonic_root;
+    int expect = 3;
+	
+    add_node(input_str_1, input_value_1, &node);
+    int actual = add_node(input_str_2, input_value_2, &node);
+	
+	assert_two_num_eq(expect, actual);
+}
+
+static void test_to_mnemonic_symbol_root() {
+	char* input_str = "m-root";
+    int expect = 1;
+	
+    int actual = to_mnemonic_symbol(input_str);
+	
+	assert_two_num_eq(expect, actual);
+    reset_trees();
+}
+
+static void test_to_mnemonic_symbol_add() {
+	char* input_str = "str";
+    int expect = 2;
+	
+    int actual = to_mnemonic_symbol(input_str);
+	
+	assert_two_num_eq(expect, actual);
+    reset_trees();
+}
+
+static void test_to_mnemonic_symbol_add_and_find() {
+	char* input_str = "str";
+    int expect = 2;
+
+	to_mnemonic_symbol(input_str);
+    int actual = to_mnemonic_symbol(input_str);
+	
+	assert_two_num_eq(expect, actual);
+    reset_trees();
+}
+
+static void test_to_mnemonic_symbol_find_intermediate() {
+	char* input_str_1 = "str";
+	char* input_str_2 = "ldr";
+    int expect = 2;
+
+	to_mnemonic_symbol(input_str_1);
+	to_mnemonic_symbol(input_str_2);
+    int actual = to_mnemonic_symbol(input_str_1);
+	
+	assert_two_num_eq(expect, actual);
+    reset_trees();
+}
+
 
 int main(int argc, char* argv[]) {
     test_find_node_no_node();
@@ -156,5 +228,10 @@ int main(int argc, char* argv[]) {
     test_find_node_no_second_node();
     test_find_node_second_node();
     test_add_node_new_node();
+    test_add_node_two_nodes();
+    test_to_mnemonic_symbol_root();
+    test_to_mnemonic_symbol_add();
+    test_to_mnemonic_symbol_add_and_find();
+    test_to_mnemonic_symbol_find_intermediate();
     return 0;
 }
