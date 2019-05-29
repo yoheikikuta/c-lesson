11a8de92a8425c3294241786affc15918e339fc4
yoheikikuta <diracdiego@gmail.com>
2019-05-29 18:21:59 +0900

    Add dictionary to handle label addresses
    Also add some utility functions comparint two Words and KeyValues.

--- sources/arm_asm/05_asm/cl_util.h
+++ sources/arm_asm/05_asm/cl_util.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "parser.h"
+#include "dict.h"
 #include <stdio.h>
 
 enum FileOpenMode { FREAD, FREADBIN, FWRITE };
@@ -8,9 +9,17 @@ enum FileOpenMode { FREAD, FREADBIN, FWRITE };
 Assert if num1 != num2.
 Assert if str1 != str2.
 Assert if str != substr.
+Return 1 if Word1 == Word2, 0 else.
+Return 1 if KevValue1 == KevValue2, 0 else.
+Assert if Word1 != Word2.
+Assert if KeyValue1 != KeyValue2.
 Return the file pointer of a given relative file path.
 */
 void assert_two_num_eq(int num1, int num2);
 void assert_two_str_eq(char* str1, char* str2);
 void assert_str_substr_eq(char* str, struct Substring* substr);
+int two_word_eq(struct Word* w1, struct Word* w2);
+int two_keyvalue_eq(struct KeyValue* kv1, struct KeyValue* kv2);
+void assert_two_word_eq(struct Word* w1, struct Word* w2);
+void assert_two_keyvalue_eq(struct KeyValue* kv1, struct KeyValue* kv2);
 FILE* get_fp(char* rel_path, enum FileOpenMode mode);
