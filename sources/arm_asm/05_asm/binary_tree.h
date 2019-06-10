2416b09660c12d6bb20f0b7d073c0f36bc47b87d
yoheikikuta <diracdiego@gmail.com>
2019-06-11 02:22:53 +0900

    [Refactoring] Use macro to specify mnemonic ids.
    Note that the order of set_mnemonics() is identical to
    that of integers defined in binary_tree.h.

--- sources/arm_asm/05_asm/binary_tree.h
+++ sources/arm_asm/05_asm/binary_tree.h
@@ -3,6 +3,13 @@
 #define NO_EXISTING_NODE -1
 #define MNEMONIC_ID_INIT 1
 #define LABEL_ID_INIT 100000
+// mnemonic ids.
+#define _MOV 1
+#define _STR 2
+#define _LDR 3
+#define _RAW 4
+#define _B 5
+#define _GLOBAL 6
 
 struct Node {
     char* name;
@@ -16,6 +23,8 @@ enum Side {LEFT, RIGHT};
 /*
 Return symbol int corresponding to a mnemonic.
 Return symbol int corresponding to a label.
+Set mnemonics symbols.
 */
 int to_mnemonic_symbol(char* str);
 int to_label_symbol(char* str);
+void set_mnemonics();
