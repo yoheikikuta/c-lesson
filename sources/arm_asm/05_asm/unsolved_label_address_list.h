847f31f75be3d40356dbbc359428537d2176bb6c
yoheikikuta <diracdiego@gmail.com>
2019-06-12 16:06:21 +0900

    [Refactoring] Extract list putting procedures as function
    Also rename linkedlist_init() as unsolved_label_address_list_init() and
    add some comments.

--- sources/arm_asm/05_asm/unsolved_label_address_list.h
+++ sources/arm_asm/05_asm/unsolved_label_address_list.h
@@ -7,6 +7,13 @@ struct LinkedList {
 	struct LinkedList* next;
 };
 
+/*
+Initialize unsolved_label_address_list.
+Put add_elem into unsolved_label_address_list.
+Create an elem from the input and put the elem into unsolved_label_address_list.
+Return 1 and get a list of unsolved_label_address_list, 0 if no list.
+*/
 void linkedlist_init();
 void linkedlist_put(struct LinkedList* add_elem);
+void common_unsolved_label_address_list_put(int emitter_pos, int symbol_label, int word);
 int linkedlist_get(struct LinkedList* out_list);
