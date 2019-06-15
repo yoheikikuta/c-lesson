330c67f9329a13a9f0ad628630738f8150c0641f
yoheikikuta <diracdiego@gmail.com>
2019-06-15 15:28:56 +0900

    [Refactoring] Make dictionary simpler to use int value
    The previous version's value is Word struct,
    but the dictionary only stores an interger as its value
    to store the position of a label.

--- sources/arm_asm/05_asm/dict.h
+++ sources/arm_asm/05_asm/dict.h
@@ -5,12 +5,12 @@
 
 struct KeyValue {
     int key;
-    struct Word value;
+    int value;
 };
 
 /*
 Put element into the dictionary.
 Retunr 1 if sucessfully get the element of the key, 0 else.
 */
-void dict_put(int key, struct Word* elem);
-int dict_get(int key, struct Word* out_elem);
+void dict_put(int key, int value);
+int dict_get(int key, int* out_value);
