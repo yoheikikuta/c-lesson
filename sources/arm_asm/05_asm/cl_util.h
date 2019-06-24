4dd4d48b91ee289e8554b814b1893e7c16698fbc
yoheikikuta <diracdiego@gmail.com>
2019-06-24 10:37:53 +0900

    Use AsmResultElem insted of Word
    "word" stands for 32 bits integer in ARM assembly.
    So use AsmResult insted of word to express results of asm_one.
    Also remove unused utility functions.

--- sources/arm_asm/05_asm/cl_util.h
+++ sources/arm_asm/05_asm/cl_util.h
@@ -11,10 +11,8 @@ enum FileOpenMode { FREAD, FREADBIN, FWRITE };
 Assert if num1 != num2.
 Assert if str1 != str2.
 Assert if str != substr.
-Return 1 if Word1 == Word2, 0 else.
 Return 1 if KevValue1 == KevValue2, 0 else.
 Return 1 if LinkedList1 == LinkedList2, 0 else.
-Assert if Word1 != Word2.
 Assert if KeyValue1 != KeyValue2.
 Assert if LinkedList1 != LinkedList2.
 Return the file pointer of a given relative file path.
@@ -22,10 +20,8 @@ Return the file pointer of a given relative file path.
 void assert_two_num_eq(int num1, int num2);
 void assert_two_str_eq(char* str1, char* str2);
 void assert_str_substr_eq(char* str, struct Substring* substr);
-int two_word_eq(struct Word* w1, struct Word* w2);
 int two_keyvalue_eq(struct KeyValue* kv1, struct KeyValue* kv2);
 int two_linkedlist_eq(struct LinkedList* list1, struct LinkedList* list2);
-void assert_two_word_eq(struct Word* w1, struct Word* w2);
 void assert_two_keyvalue_eq(struct KeyValue* kv1, struct KeyValue* kv2);
 void assert_two_linkedlist_eq(struct LinkedList* list1, struct LinkedList* list2);
 FILE* get_fp(char* rel_path, enum FileOpenMode mode);
