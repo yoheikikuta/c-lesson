8b35d3ab28d2d44d29c0298f7333f4d40aad814b
yoheikikuta <diracdiego@gmail.com>
2019-06-25 19:32:25 +0900

    Modify main.s to print Hello World
    Set label address into r0 register.

--- sources/casm_link/01_call_c/1_2_call_c_msg/main.s
+++ sources/casm_link/01_call_c/1_2_call_c_msg/main.s
@@ -1,9 +1,7 @@
 .globl _start
 _start:
     ldr r13,=0x07FFFFFF
-    // TODO: ここに何かを書いて、下のprint_msgで
-    // Hello Worldと表示するようにしてください。
-    // 
+    ldr r0,=msg
     bl print_msg
 loop:
     b loop
