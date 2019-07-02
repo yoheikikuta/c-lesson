6af4780cf53a7774684269b86c9d714d62e88e8a
yoheikikuta <diracdiego@gmail.com>
2019-07-02 22:53:52 +0900

    Implement mov r0, #X instruction
    Use emitter to store binaries and define set_binaries
    to set the obtained binaries into binary_buf.

--- sources/casm_link/06_jit_ps/ps_jit.c
+++ sources/casm_link/06_jit_ps/ps_jit.c
@@ -7,6 +7,21 @@
 #include "test_util.h"
 
 extern int eval(int r0, int r1, char *str);
+struct Emitter {
+    unsigned char* buf;
+    int pos;
+};
+static struct Emitter emitter;
+static unsigned char byte_buf[1024];
+
+// number: 0xE59F101E -> emitter: 
+//   pos       n     n+1   n+2   n+3   n+4
+//   buf  0x1E, 0x10, 0x9F, 0xE5
+void emit_int(struct Emitter* emitter, int number) {
+    for (int i = 0; i < 4; i++) {
+        emitter->buf[emitter->pos++] = (number >> (i * 8)) & 0xFF;
+    }
+}
 
 /*
 JIT
@@ -25,19 +40,46 @@ void ensure_jit_buf() {
     }
 }
 
+void set_binaries(struct Emitter* emitter) {
+    for (int i = 0; i < emitter->pos; i = i + 4) {
+        int word = 0x0;
+        for (int j = 3; j >= 0; j--) {
+            word += emitter->buf[j + i] << (j * 8);
+        }
+        binary_buf[i] = word;
+    }
+}
+
 int* jit_script(char *input) {
     ensure_jit_buf();
+    emitter.buf = byte_buf;
+    emitter.pos = 0;
     /*
     Function only returns 3.
     */
-    binary_buf[0] = 0xe3a00003;  // mov r0, #0x3
-    binary_buf[1] = 0xe1a0f00e;  // mov r15, r14
+    struct Substr remain={input, strlen(input)};
+    int word;
 
+    while (!is_end(&remain)) {
+        skip_space(&remain);
+        if (is_number(remain.ptr)) {
+            word = 0xE3A00000 + parse_number(remain.ptr);  // mov r0, #X
+            emit_int(&emitter, word);
+            skip_token(&remain);
+            continue;
+        } else {
+            continue;
+        }
+    }
+    word = 0xe1a0f00e;  // mov r15, r14
+    emit_int(&emitter, word);
+
+    set_binaries(&emitter);
     return binary_buf;
 }
 
 void test_jit_sctipr_only_3() {
-    char* input = "0";
+    char* input = "3";
     int expect = 3;
 
     int (*funcvar)(int, int);
